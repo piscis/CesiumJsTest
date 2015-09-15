9371c15ab9e77e4b064dc00ea1726afa7e2df3a2
Alexander Pirsig <self@pirsig.net>
2015-09-15 18:33:53 +0200

    license + polyline example

--- app/public/js/main.ts
+++ app/public/js/main.ts
@@ -4,5 +4,19 @@ declare var Cesium:any;
 
 window.onload = ()=>{
     console.log('onload');
-    var viewer = new Cesium.Viewer('cesiumContainer');
+    var viewer = new Cesium.Viewer('cesiumContainer',{
+        animation : false,
+        timeline : false
+    });
+
+
+    var redLine = viewer.entities.add({
+        name : 'Red line on the surface',
+        polyline : {
+            positions : Cesium.Cartesian3.fromDegreesArray([48.12, 11.61,
+                -125, 35]),
+            width : 3,
+            material : Cesium.Color.RED
+        }
+    });
 }
